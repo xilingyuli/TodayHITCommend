6799eb12f6ce88c6e9232f2b58f3c902fadc1f69
xilingyuli <786979248@qq.com>
2015-12-22 13:49:20 +0800

    final

---
+++ 刷票软件c#源码by赵璐媛/CSharp_Task3/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CSharp_Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(36, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请键入新闻页面地址";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(96, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "确定";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(40, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 64);
+            this.label2.TabIndex = 4;
+            this.label2.Text = " ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(96, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(36, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "次数";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "刷票软件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 刷票软件c#源码by赵璐媛/CSharp_Task3/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace CSharp_Task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //给刷票地址url发送times次请求
+        private int vote(String url,int times)
+        {
+            if (url == null||times == -1)  //地址无效
+                return 0;
+            try
+            {
+                Random r = new Random();
+                for (int i = 1; i <= times; i++)
+                {
+                    HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                    req.Method = "GET";
+                    req.Headers["X-Forwarded-For"] = "202." + r.Next(256) + "." + r.Next(256) + "." + r.Next(256);
+                    req.Timeout = 2000;
+                    req.GetResponse().Close();
+                    label2.Text = "第" + i + "次刷票";
+                    this.Refresh();
+                    //Thread.Sleep(1000);  //两次刷票间延时
+                }
+            }catch(Exception e)  //发生异常
+            {
+                return -1;
+            }
+            return 1;  //正常退出
+        }
+
+        //获取页面文本
+        private String webCatch(String url)
+        {
+            if (url == null || url.Equals(""))
+                return null;
+            String result;
+            Encoding myEncoding = Encoding.GetEncoding("gb2312");
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+            try
+            {
+                HttpWebResponse myResponse = (HttpWebResponse)req.GetResponse();
+                StreamReader reader = new StreamReader(myResponse.GetResponseStream(), myEncoding);
+                result = reader.ReadToEnd();
+                reader.Close();
+            }
+            catch (System.Net.WebException e)
+            {
+                return null;
+            }
+            return result;
+        }
+
+        //获取刷票使用的地址
+        private String getCommendUrl(String result)
+        {
+            if (result != null && result.IndexOf("/commend/") >= 0)
+                return "http://today.hit.edu.cn" + result.Substring(result.IndexOf("/commend/"), 16) + "1.htm";
+            else
+                return null;
+        }
+
+        //获取已刷的次数
+        private int getCommendNum(String result)
+        {
+            try
+            {
+                String[] lines = result.Split('\n');
+                return int.Parse(lines[35]);
+            }catch(Exception e)
+            {
+                return -1;
+            }
+
+        }
+
+        //获取想要刷票的次数
+        private int getRepeatNum(String num)
+        {
+            try
+            {
+                return int.Parse(num);
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+
+        //刷票线程
+        private void run()
+        {
+            String url = getCommendUrl(webCatch(textBox1.Text));
+            int t = vote(url,getRepeatNum(textBox2.Text));
+            if (t == 1)
+                label2.Text = "刷票成功，现在推荐量为"+ getCommendNum(webCatch(url));
+            else if (t == 0)
+                label2.Text = "不正确的地址或次数";
+            else if (t == -1)
+                label2.Text = "刷票过程中出现网络异常，现在推荐量为" + getCommendNum(webCatch(url));
+        }
+
+        //点击则刷票
+        private void button2_Click(object sender, EventArgs e)
+        {
+            run();
+        }
+
+    }
+}
